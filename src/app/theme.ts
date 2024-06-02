084242c3914d1bb2afa0b7ff74436c44232665df
Armin <119794879+Entropizm@users.noreply.github.com>
2024-06-02 11:15:31 +0200

    Revert "Update theme.ts"
    This reverts commit 0d8496f6050387675ad9ab4743e9e85b934fb9d7.

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme } from '@chakra-ui/react'
 
 export const themeConfig = {
   config: {
-    initialColorMode: 'light',
+    initialColorMode: 'dark',
     useSystemColorMode: false,
   },
 }
